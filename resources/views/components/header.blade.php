c4c50865323cfeb934e96c335a217dd07ef492c4
mikamika2ri@gmail.com <kondomika@konnoMacBook-Pro-2.local>
2021-04-04 23:19:01 +0900

    Design first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header id="header">
+    <p class="logo"><a href="{{ url('/') }}"><img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}"></a></p>
+    <ul class="nav">
+        @guest
+            @if (Route::has('login'))
+                <li><a href="{{ route('login') }}" class="icon login">ログイン</a></li>
+            @endif
+        @else
+            <li><span id="menuDrawer" class="icon menu">メニュー</span></li>
+            <div id="menuDrawerContent">
+
+            </div>
+        @endguest
+    </ul>
+</header>

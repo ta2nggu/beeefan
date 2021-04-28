a400911924e9d0dd2631d94e30291dfaa42c9654
“ta2nggu“ <ta2nggu@gmail.com>
2021-04-29 01:04:08 +0900

    결제 테스트

--- app/Http/Controllers/CurlController.php
+++ app/Http/Controllers/CurlController.php
@@ -11,18 +11,20 @@ class CurlController extends Controller
 
     }
 
-    public static function postCurl() {
+    public function postCurl() {
+        $ORD = 'A015';
+
         $curl=curl_init();
         curl_setopt( $curl, CURLOPT_POST, true );
         curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
         curl_setopt( $curl, CURLOPT_URL, 'https://pt01.mul-pay.jp/payment/EntryTran.idPass' );
         $param = [
-            'ShopID'       => 'ADMINISTRATOR',//'YourShopId',
-            'ShopPass'     => 'Kg3uqNF9?',//'YourShopPassword',
-            'OrderID'      => 'A001',//'Orderid',
+            'ShopID'       => 'tshop00050389',//'YourShopId',
+            'ShopPass'     => '67knmfpc',//'YourShopPassword',
+            'OrderID'      => $ORD,//'A009',//'Orderid',
             'JobCd'        => 'AUTH',
             //'ItemCode'     => '0000990',
-            'Amount'       => '100',
+            'Amount'       => '30',
             'Tax'          => '0',//'10',
             'TdFlag'       => '1',
             //'TdTenantName' => 'YourTenantName',
@@ -49,17 +51,66 @@ class CurlController extends Controller
             return false;
         }
 
+//        return dd($data['AccessPass']);AccessID
+
         // 正常
-        return true;
+        //return true;
+        $this->pay($ORD, $data['AccessID'], $data['AccessPass']);
     }
 
-    public static function getCurl($host, $url) {
-        $ch = curl_init($host . $url);
+    public function pay($ORD, $ID, $PASS) {
+        // リクエストコネクションの設定
+        $curl=curl_init();
+        curl_setopt( $curl, CURLOPT_POST, true );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_URL, 'https://pt01.mul-pay.jp/payment/ExecTran.idPass' );
+        // 本人認証サービス使用,トークン使用時
+        $param = [
+            'AccessID'        => $ID,//'SampleAccessID',
+            'AccessPass'      => $PASS,//'SampleAccessPass',
+            'OrderID'         => $ORD,//'SampleOrderID',
+            'Method'          => '2',
+            'PayTimes'        => '2',//'2',
+            //'Token'           => 'SampleToken',
+            //'HttpAccept'      => 'SampleHttpAccept',
+            //'HttpUserAgent'   => 'HttpUserAgent',
+            //'DeviceCategory'  => '0',
+            //'ClientField1'    => 'SampleClientField1',
+            //'ClientField2'    => 'SampleClientField2',
+            //'ClientField3'    => 'SampleClientField3',
 
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-        $result = curl_exec($ch);
-        curl_close($ch);
+            'CardNo' => '5461122021371243',
+            'Expire' => '2303',
 
-        return $result;
+            'ClientFieldFlag' => '0',
+            'TokenType'       => '1',
+            'RetUrl'          => 'https://google.com',//'https://example.com/xxxxx'
+        ];
+        // リクエストボディの生成
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, http_build_query( $param ) );
+
+        // リクエスト送信
+        $response = curl_exec( $curl );
+        $curlinfo = curl_getinfo( $curl );
+        curl_close( $curl );
+
+        // レスポンスチェック
+        if( $curlinfo[ 'http_code' ] != 200 ){
+            // エラー
+            return false;
+        }
+
+        // レスポンスのエラーチェック
+        parse_str( $response, $data );
+        if( array_key_exists( 'ErrCode', $data ) ){
+            // エラー
+            return dd($data);
+            return false;
+        }
+
+        return dd($data);
+
+        // 正常
+        return true;
     }
 }

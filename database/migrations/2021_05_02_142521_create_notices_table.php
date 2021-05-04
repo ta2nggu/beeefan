bbb419c126bd4593d9a4156f04452c3bd96b3c75
“ta2nggu“ <ta2nggu@gmail.com>
2021-05-04 23:27:29 +0900

    공지사항 개발 시작2

---
+++ database/migrations/2021_05_02_142521_create_notices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('from_user_id');
+            $table->bigInteger('to_user_id')->nullable();
+            $table->string('title', 400);
+            $table->text('body');
+            $table->boolean('del')->default(0);//1 삭제, 0 미삭제
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -48,6 +48,27 @@
                 </dl>
             </div>
         </div>
+<<<<<<< HEAD
+=======
+{{--        21.05.03 김태영, 공지사항    --}}
+        <div><a href="/admin/">{{__('공지사항')}}</a></div>
+        <div><a href="/admin/creatorReg">{{__('クリエイター新規登録크리에이터신규등록')}}</a></div>
+        <div>{{ __('クリエイター名で検索する 크리에이터 이름 검색') }}</div>
+        <input id="search_creator_input" type="text" placeholder="nicknameを入力してください。">
+        <div>{{ __('sorting 정렬') }}</div>
+        <select id="search_creator_select" name="sortCreators">
+            {{--                                <option value="" selected disabled hidden>お選びください.</option>--}}
+            <option value="{{ __("created_at,desc") }}">{{ __('登錄日降順 등록일 내림차순') }}</option>
+            <option value="{{ __("created_at,asc") }}">{{ __('登錄日昇順 등록일 오름차순') }}</option>
+        </select>
+
+        <div class="post-data">
+            @include('admin/indexData')
+        </div>
+        <div class="ajax-load text-center">
+            <p><img src="{{ asset('storage/images/loading.gif') }}"/>データを持ってきています。</p>
+        </div>
+>>>>>>> 084b449... 공지사항 개발 시작
 
         <div id="creatorRegBtn" class="btnBox">
             <p><a href="{{ url('/admin/creatorReg') }}" class="btn btnAd">クリエイター新規登録</a></p>
